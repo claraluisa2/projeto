5d17ad11f29adf3bb6326ef202952ecaf9d090c6
Clara Luisa <claraluisa2@gmail.com>
2025-06-12 20:23:51 -0300

    first commit

--- controle-de-impedancia.c
+++ controle-de-impedancia.c
@@ -8,7 +8,7 @@
 
 double impedance_control(double t, double k, double b, double teta_eq, double amplitude, double freq) {
     double teta = amplitude * sin(2 * PI * freq * t); // posição da junta no tempo t
-    double dteta = 2 * PI * freq * amplitude * cos(2 * PI * freq * t); // velocidade da junta no tempo t (Seria melhor calcular pela derivada forma numérica?)
+    double dteta = 2 * PI * freq * amplitude * cos(2 * PI * freq * t); //velocidade da junta no tempo t
     double torque = k * (teta - teta_eq) - b * dteta;
     return torque;
 }
@@ -30,7 +30,7 @@ int main() {
     }
 
     printf("time\ttorque\n");
-    for (int i = 0; i < 801; i++) {
+    for (int i = 0; i < N; i++) {
         printf("%.4f\t%.4f\n", time[i], torque[i]);
     }
 
